4acaac4c5bb5445bfcd8818c6402ae9570a1352e
samarth1301 <samarthsingh890.ss@gmail.com>
2021-08-15 12:25:38 +0530

    spelling check

--- JS/index.js
+++ JS/index.js
@@ -1,66 +1,65 @@
 const addNote=document.getElementById('addNote');
-
 const noteArr=[];
 
 // console.log(addNote);
 let count=0;
 addNote.addEventListener('click',()=>{
     // localStorage.setItem(`key${count+1}`,value);
-   
+    
     let value=String(document.getElementById('noteInput').value);
     if(value.length>0){
         count++;
         if(count>0){
             document.getElementById('desc').className='hide';
         }
-    
-    document.getElementById('noteInput').value='';
-    // console.log(typeof(value));
-    let element=document.createElement('div');
-    element.className='note';
-    element.id=`${noteArr.length+1}`;
-    let elementHead=document.createElement('div');
-    let textNode=document.createTextNode(`Note: ${noteArr.length+1}`);
-    let h2=document.createElement('h2');
-    h2.id=`head${noteArr.length+1}`
-    h2.appendChild(textNode);
-    elementHead.appendChild(h2);
-    elementHead.className='noteHead';
-    let elementBody=document.createElement('div');
-    textNode=document.createTextNode(value);
-    let p=document.createElement('p');
-    p.appendChild(textNode);
-    elementBody.appendChild(p);
-    // elementBody.innerText=String(value);
-    elementBody.className='noteBody';
-    
-
-    let button=document.createElement('button');
-    button.setAttribute('type','button');
-    button.className='button';
-    button.classList.add('dltBtn');
-    button.innerText='Delete note';
-    button.id=`delBtn${noteArr.length+1}`;
-    temp=button.id;
-    
-    button.setAttribute('onclick',`func(${noteArr.length+1})`);
-    // console.log(button.id);
-    
-    
-    
-    // console.log(elementBody);
-    element.appendChild(elementHead);
-    element.appendChild(elementBody);
-    element.appendChild(button);
-    
-    // document.getElementById('notesContainer').appendChild(element);
-    noteArr.push(element);
-    // document.getElementById('notesContainer').appendChild(noteArr[0]);
-   
-    
-    document.getElementById('notesContainer').appendChild(element);
-    
-}
+        
+        document.getElementById('noteInput').value='';
+        // console.log(typeof(value));
+        let element=document.createElement('div');
+        element.className='note';
+        element.id=`${noteArr.length+1}`;
+        let elementHead=document.createElement('div');
+        let textNode=document.createTextNode(`Note: ${noteArr.length+1}`);
+        let h2=document.createElement('h2');
+        h2.id=`head${noteArr.length+1}`
+        h2.appendChild(textNode);
+        elementHead.appendChild(h2);
+        elementHead.className='noteHead';
+        let elementBody=document.createElement('div');
+        textNode=document.createTextNode(value);
+        let p=document.createElement('p');
+        p.appendChild(textNode);
+        elementBody.appendChild(p);
+        // elementBody.innerText=String(value);
+        elementBody.className='noteBody';
+        
+        
+        let button=document.createElement('button');
+        button.setAttribute('type','button');
+        button.className='button';
+        button.classList.add('dltBtn');
+        button.innerText='Delete note';
+        button.id=`delBtn${noteArr.length+1}`;
+        temp=button.id;
+        
+        button.setAttribute('onclick',`func(${noteArr.length+1})`);
+        // console.log(button.id);
+        
+        
+        
+        // console.log(elementBody);
+        element.appendChild(elementHead);
+        element.appendChild(elementBody);
+        element.appendChild(button);
+        
+        // document.getElementById('notesContainer').appendChild(element);
+        noteArr.push(element);
+        // document.getElementById('notesContainer').appendChild(noteArr[0]);
+        
+        
+        document.getElementById('notesContainer').appendChild(element);
+        
+    }
 });
 
 //id u want to submit on pressing enter
@@ -84,37 +83,37 @@ function func(id){
     // document.getElementById('notesContainer').removeChild(getElementById(String(id)));
     
     let element=document.getElementById(String(id));
-   
+    
     document.getElementById('notesContainer').removeChild(element);
     let temp=-1;
     
-  noteArr.forEach((e,i)=>{
-      if(e===element){
-          temp=i;
-         
-
-      }
-       if(i>temp && temp!=-1 ){
-         
-          element=document.getElementById(`head${i+1}`);
-          element.id=`head${i}`;
-          element.innerText=`Note:${i}`
-          element=document.getElementById(`${i+1}`);
-          element.id=`${i}`;
-          let button=document.getElementById(`delBtn${i+1}`)
-          button.id=`delBtn${i}`;
-          button.setAttribute('onclick',`func(${i})`);
+    noteArr.forEach((e,i)=>{
+        if(e===element){
+            temp=i;
+            
+            
+        }
+        if(i>temp && temp!=-1 ){
+            
+            element=document.getElementById(`head${i+1}`);
+            element.id=`head${i}`;
+            element.innerText=`Note:${i}`
+            element=document.getElementById(`${i+1}`);
+            element.id=`${i}`;
+            let button=document.getElementById(`delBtn${i+1}`)
+            button.id=`delBtn${i}`;
+            button.setAttribute('onclick',`func(${i})`);
         }
     });
     if(temp>-1){
         noteArr.splice(temp,1);
     }
-
-if(noteArr.length===0){
-    document.getElementById('desc').classList.remove("hide");
-}
-// document.getElementById(`head${noteArr.length}`).innerHTML=`Note:${noteArr.length}`;
-
+    
+    if(noteArr.length===0){
+        document.getElementById('desc').classList.remove("hide");
+    }
+    // document.getElementById(`head${noteArr.length}`).innerHTML=`Note:${noteArr.length}`;
+    
 };
 
 let element=document.createElement('h2');
@@ -127,12 +126,12 @@ const searchNote=document.getElementById('searchText');
 searchNote.addEventListener('input',()=>{
     const notesContainer=document.getElementById('notesContainer');
     const value=searchNote.value;
-//    if(value.length>0){ noteArr.forEach((e,i)=>{   
-//        notesContainer.removeChild((document.getElementById(`${i+1}`)));
-//     });}
-    // console.log(searchNote.value,value.length);
-    if(value.length===0){
-        noteArr.forEach((e)=>{
+    //    if(value.length>0){ noteArr.forEach((e,i)=>{   
+        //        notesContainer.removeChild((document.getElementById(`${i+1}`)));
+        //     });}
+        // console.log(searchNote.value,value.length);
+        if(value.length===0){
+            noteArr.forEach((e)=>{
                 // document.getElementById('notesContainer').appendChild(e);
                 e.classList.remove('hide');
             });
@@ -156,23 +155,25 @@ searchNote.addEventListener('input',()=>{
                 
             }
             if(newArr.length>0){
-            element.classList.add('hide');
-            console.log(newArr);
-            noteArr.forEach((e)=>{   
-                // notesContainer.removeChild((e));
-                e.classList.add('hide');
-            });
-            newArr.forEach((e)=>{
-                // document.getElementById('notesContainer').appendChild(e);
-                e.classList.remove('hide');
-            });
-            // notesContainer.removeChild(document.getElementById("notFound"));
-    }
-    };
+                element.classList.add('hide');
+                console.log(newArr);
+                noteArr.forEach((e)=>{   
+                    // notesContainer.removeChild((e));
+                    e.classList.add('hide');
+                });
+                newArr.forEach((e)=>{
+                    // document.getElementById('notesContainer').appendChild(e);
+                    e.classList.remove('hide');
+                });
+                // notesContainer.removeChild(document.getElementById("notFound"));
+            }
+        };
+        
+    });
     
-});
-
-
-
-
-
+    
+    
+    
+    
+    
+    
